c0f0b2ee9f34ce7bd98a246d8e4b30562637e1b0
1mosce <shuliaknikolay@gmail.com>
2022-06-18 13:39:55 +0300

    Restaraunt controller added

--- Models/Restaraunt.cs
+++ Models/Restaraunt.cs
@@ -2,9 +2,13 @@
 {
     public class Restaraunt
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Address { get; set; }
         public int Rating { get; set; }
-        
+        public string Logo { get; set; }
+
+        //Inner Data 
+        public ICollection<Menu> Menu { get; set; }   
     }
 }
